0e05bd8961ee783cd0b98c42e751e03b6ebc5d74
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-05 18:59:18 +0800

    page-leave-active

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,14 @@
     <NuxtLink to="/about">About page</NuxtLink>
   </div>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 3s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
