8dc07b5cf88998c0233632c83013ccc97f3b9638
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-06 18:31:16 +0800

    Updates

--- _nuxt/BsExYjqh.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./COF3ZOpx.js";import{c as t,a as s,t as o,u as r,o as c}from"./BjBrioyg.js";const _={__name:"hello",setup(l){const{data:e}=a("/api/hello","$UtSbjGhzPc");return(n,u)=>(c(),t("div",null,[s("h1",null,o(r(e)),1)]))}};export{_ as default};
