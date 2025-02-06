48b4efae2978f1466b6bbcb8a20f60426388aa55
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-06 18:27:29 +0800

    post

---
+++ pages/user.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>name: {{ user.name }}</div>
+    <div>age: {{ user.age }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data: user } = await useFetch('/api/user', {
+  method: 'post',
+  body: {
+    name: 'Daniel',
+    age: 18
+  }
+});
+</script>
