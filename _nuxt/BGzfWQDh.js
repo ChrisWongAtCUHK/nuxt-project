9c353f05bc589e1cb42b19e128e76bb0b877af01
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-06 13:59:59 +0800

    Updates

--- _nuxt/BGzfWQDh.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./CMRqXNN5.js";import{_ as n,c as a,a as r,b as s,w as _,o as c,d}from"./Czm_Raz8.js";const u={};function l(m,e){const t=o;return c(),a("div",null,[e[1]||(e[1]=r("h1",null,"Home page",-1)),s(t,{to:"/about"},{default:_(()=>e[0]||(e[0]=[d("About page")])),_:1})])}const i=n(u,[["render",l]]);export{i as default};
